927d7b5c55788965aae5e32adba27d84d649be4b
tranquilshore <sahilgaucho@gmail.com>
2019-06-28 08:52:03 +0530

    stack and queue

--- circular_tour.py
+++ circular_tour.py
@@ -1,5 +1,5 @@
 '''
-https://www.geeksforgeeks.org/find-a-tour-that-visits-all-stations/
+https://leetcode.com/problems/gas-station/discuss/319512/O(n)-complexity-solution.
 Suppose there is a circle. There are n petrol pumps on that circle. You are given two sets of data.
 1. The amount of petrol that every petrol pump has.
 2. Distance from that petrol pump to the next petrol pump.
@@ -7,36 +7,33 @@ Suppose there is a circle. There are n petrol pumps on that circle. You are give
 Calculate the first point from where a truck will be able to complete the circle (The truck will stop at each petrol pump and it has infinite capacity). Expected time complexity is O(n). Assume for 1 litre petrol, the truck can go 1 unit of distance.
 
 For example, let there be 4 petrol pumps with amount of petrol and distance to next petrol pump value pairs as {4, 6}, {6, 5}, {7, 3} and {4, 5}. The first point from where truck can make a circular tour is 2nd petrol pump. Output should be start = 1 (index of 2nd petrol pump).
+
+couple of points:
+
+1. we first check if a solution exists by summing all gas values and all cost values and checking if sum(gas)>sum(cost). If this condition is satisfied, we are guaranteed a unique solution by the problem.
+2. find the starting poiint.
 '''
+petrol = [4,6,7,4]
+distance = [6,5,3,5]
 
-class petrol_pump:
-    def __init__(self,petrol,distance):
-        self.petrol = petrol
-        self.distance = distance
+n = len(petrol)
 
-def start_of_tour(a):
-    n = len(a)
+def circular_tour(petrol,distance,n):
+    if sum(petrol) < sum(distance):
+        return -1 
 
-    #consider first as start and second as the end
-    start = 0 
-    end = 1
+    balance = 0
+    pos = 0
+    for i in range(n):
+        balance += petrol[i] - distance[i]
+        if balance<0:
+            pos = i+1 
+            balance = 0
+    
+    return pos 
 
-    curr_petrol = a[start].petrol - a[start].distance
-    #while all pumps are not visited 
-    while start != end:
-        while start != end and curr_petrol < 0:
-            curr_petrol -= a[start].petrol - a[start].distance
-            start = (start+1)%n 
-            #zero is considered as start again, so no possible solution
-            if start == 0:
-                return -1 
-        curr_petrol += a[end].petrol - a[end].distance
-        end = (end+1)%n 
-    return start 
+print circular_tour(petrol,distance,n)
 
-a = [petrol_pump(6,4), petrol_pump(3,6),petrol_pump(7,3)]
-a = [petrol_pump(4,6), petrol_pump(6,5), petrol_pump(7,3), petrol_pump(4,5)]
-print start_of_tour(a)
 
 
 
