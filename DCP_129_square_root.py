1cc2091be31177dee1f9222a8581b193a1c55597
tranquilshore <sahilgaucho@gmail.com>
2019-08-20 12:18:20 +0530

    sqaure root binary search

---
+++ DCP_129_square_root.py
@@ -0,0 +1,19 @@
+n = 35
+
+def square_root(n):
+    if n == 0 or n == 1:
+        return n 
+    start = 1
+    end = n 
+    while start <= end:
+        mid = (start+end)/2 
+        if mid*mid == n:
+            return mid 
+        if mid*mid < n:
+            start = mid+1
+            ans = mid 
+        else:
+            end = mid-1
+    return ans 
+
+print square_root(n)
